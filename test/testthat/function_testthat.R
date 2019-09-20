41aebfc1ff0da8e50c4f7967e2a3004ddb7cd7d6
marcelamu95 <lgarzon@grupobit.net>
2019-09-20 17:58:30 -0500

    test created

--- test/testthat/function_testthat.R
+++ test/testthat/function_testthat.R
@@ -1,11 +1,12 @@
 x <- setwd(system.file("extdata", package = "packagefars"))
+
 year <- 2013
 test_that("package correct",{
   testthat::expect_is(fars_read("accident_2013.csv.bz2"),
                  c("tbl_df", "tbl", "data.frame"))
   testthat::expect_equal(make_filename(year), paste0("accident_",year,".csv.bz2"))
   testthat::expect_is(make_filename(year), "character")
-  testthat::expect_is(fars_map_state(12, year), "NULL")
+
 })
 
 setwd(x)
