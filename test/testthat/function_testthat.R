45c94bbd2851a34ad3773ac6e6954393f526e37a
marcela <marcelamu9@gmail.com>
2019-09-21 10:44:59 -0500

    delete data

--- test/testthat/function_testthat.R
+++ test/testthat/function_testthat.R
@@ -1,12 +1,24 @@
 x <- setwd(system.file("extdata", package = "packagefars"))
 
-year <- 2013
-test_that("package correct",{
+test_that("data files are available", {
+  testthat::expect_equal(list.files(system.file("extdata", package = "packagefars")),
+                         c("accident_2013.csv.bz2",
+                           "accident_2014.csv.bz2",
+                           "accident_2015.csv.bz2"))
+
   testthat::expect_is(fars_read("accident_2013.csv.bz2"),
-                 c("tbl_df", "tbl", "data.frame"))
-  testthat::expect_equal(make_filename(year), paste0("accident_",year,".csv.bz2"))
-  testthat::expect_is(make_filename(year), "character")
+                               c("tbl_df", "tbl", "data.frame"))
 
 })
 
-setwd(x)
+#
+# year <- 2013
+# test_that("package correct",{
+#   testthat::expect_is(fars_read("accident_2013.csv.bz2"),
+#                  c("tbl_df", "tbl", "data.frame"))
+#   testthat::expect_equal(make_filename(year), paste0("accident_",year,".csv.bz2"))
+#   testthat::expect_is(make_filename(year), "character")
+#
+# })
+#
+# setwd(x)
